a2ff7d195f5f1f30de84c897f9e005c2ba7ad4bc
edillenb <elio.dillenberg@gmail.com>
2019-12-08 17:58:09 +0100

    works now with possible selection of language, could add as many keywords as I want

--- internship_scrapy/internship_scrapy.py
+++ internship_scrapy/internship_scrapy.py
@@ -1,10 +1,10 @@
 import sys
 import os
 
-def parse_input(user_input):
+def parse_input(user_input, len):
     try:
         user_input = int(user_input)
-        assert user_input == 0 # need to link this to a list of possible choices
+        assert user_input < len
     except AssertionError:
         print("Le numero fourni ne correspond à aucun des choix proposés")
         sys.exit()
@@ -19,22 +19,28 @@ def parse_input(user_input):
 spiders = list()
 spiders.append("WelcomeToTheJungle")
 
+languages = list()
+languages.append("Python")
+languages.append("PHP")
+languages.append("Javascript")
+
 # prompt user for website to scrape -> retuns index of corresponding website
-def get_user_input(spiders):
+def get_user_input(spiders, languages):
     print ("Veuillez sélectionner un des sites suivant à scraper \n")
     for i, spider in enumerate(spiders):
-        print(str(i) + " : " + spider)
+        print(str(i) + " : " + spider + "\n")
     c_site = input("Choice : ")
-    parse_input(c_site)
-    return int(c_site)
+    parse_input(c_site, len(spiders))
+    print ("\nVeuillez un des langages suivants \n")
+    for i, language in enumerate(languages):
+        print(str(i) + " : " + language + "\n")
+    c_language = input("Choice : ")
+    parse_input(c_language, len(languages))
 
-user_choice = get_user_input(spiders)
+    return int(c_site), int(c_language)
+
+user_choice = get_user_input(spiders, languages)
 
 # calling scrapy from system to start crawling user choice / comment if you want to scrap all with bellow
-scrapy_call = "scrapy crawl " + spiders[user_choice] + " -o " + spiders[user_choice] + ".json -t json"
+scrapy_call = "scrapy crawl " + spiders[user_choice[0]] + " -a language=" + languages[user_choice[1]] + " -o " + spiders[user_choice[0]] + ".json -t json"
 os.system(scrapy_call)
-
-# Uncomment below to execute script on all websites (need to comment the two lines above)
-# for spider in spiders:
-#     scrapy_call = "scrapy crawl " + spider + " -o " + spider + ".json -t json"
-#     os.system(scrapy_call)
--- internship_scrapy/internship_scrapy/spiders/WelcomeToTheJungle.py
+++ internship_scrapy/internship_scrapy/spiders/WelcomeToTheJungle.py
@@ -1,40 +1,26 @@
 import scrapy
+import selenium
 
 class   WelcomeToTheJungle(scrapy.Spider):
     name = "WelcomeToTheJungle"
 
     start_urls= [
-        "https://www.welcometothejungle.com/fr/jobs?refinementList%5Bcontract_type_names.fr%5D%5B%5D=Stage&refinementList%5Bprofession_name.fr.Tech%5D%5B%5D=Dev%20Fullstack&refinementList%5Bprofession_name.fr.Tech%5D%5B%5D=Dev%20Backend&refinementList%5Bprofession_name.fr.Tech%5D%5B%5D=Data%20Science&refinementList%5Bprofession_name.fr.Tech%5D%5B%5D=Autres&refinementList%5Bprofession_name.fr.Tech%5D%5B%5D=Recherche%20%2F%20R%26D&refinementList%5Bprofession_name.fr.Tech%5D%5B%5D=Data%20Analysis&refinementList%5Bprofession_name.fr.Tech%5D%5B%5D=Dev%20Frontend&refinementList%5Bprofession_name.fr.Tech%5D%5B%5D=Data%20Engineering&page=1&configure%5Bfilters%5D=website.reference%3Awttj_fr&configure%5BhitsPerPage%5D=30&aroundLatLng=48.8546%2C2.3477&aroundQuery=Paris%2C%20France&aroundRadius=20000&aroundPrecision=20000"
+        "https://www.welcometothejungle.com/fr/jobs?refinementList%5Bcontract_type_names.fr%5D%5B%5D=Stage&refinementList%5Bprofession_name.fr.Tech%5D%5B%5D=Dev%20Fullstack&refinementList%5Bprofession_name.fr.Tech%5D%5B%5D=Dev%20Backend&refinementList%5Bprofession_name.fr.Tech%5D%5B%5D=DevOps%20%2F%20Infra&refinementList%5Bprofession_name.fr.Tech%5D%5B%5D=Data%20Science&refinementList%5Bprofession_name.fr.Tech%5D%5B%5D=Autres&refinementList%5Bprofession_name.fr.Tech%5D%5B%5D=Recherche%20%2F%20R%26D&refinementList%5Bprofession_name.fr.Tech%5D%5B%5D=Data%20Analysis&refinementList%5Bprofession_name.fr.Tech%5D%5B%5D=Data%20Engineering&page=1&configure%5Bfilters%5D=website.reference%3Awttj_fr&configure%5BhitsPerPage%5D=30&aroundLatLng=48.8546%2C2.3477&aroundQuery=Paris%2C%20France&aroundRadius=20000&aroundPrecision=20000"
     ]
 
     def parse(self, response):
         for link in response.xpath("//ul[@class='ais-Hits-list']/li/article/a"):
             offer = link.xpath(".//@href").get()
             yield response.follow(offer, callback=self.parse_offer)
-            yield {
-                'offer': "https://www.welcometothejungle.com" + link.xpath(".//@href").get()
-            }
-            # alternative that enters each link to parse shiet
-            # offer = link.xpath(".//@href")
-            # yield response.follow(offer, callback=self.parse_offer)
-
+        
         next_page = response.xpath("//li[@class='ais-Pagination-item ais-Pagination-item--nextPage']/a/@href").get()
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
 
     def parse_offer(self, response):
-        # plain_text = response.xpath("//body//text").get()
-        # print(plain_text)
-        # ''.join(response.select("//body//text()").extract()).strip()
-        # yield {
-        #     'data': response.xpath("//body//text").get()
-        # }
-
-        yield {
-            'offer': response.request.url
-        }
-        # for link in response.xpath("//body"):
-        #     yield {
-        #         'link': response.request.url
-        #     } 
-        
+        text = response.xpath("//div[@class='sc-11unfkk-2 NggfW']//text()").getall()
+        joined_text = ''.join(text)
+        if self.language in joined_text:
+            yield {
+                self.language + ' offer': response.request.url,
+            }
